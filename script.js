d3a8acb713e4964fca63e96528b00fcdb7e16adb
karolbrol <134250879+karolbrol@users.noreply.github.com>
2023-10-26 07:01:03 +0200

    Create playARound() function parameters validity checking

--- script.js
+++ script.js
@@ -10,14 +10,16 @@ function getComputerChoice() {
 //Play a round function -case insensitive, returning string with result eg. "You Lose! Paper beats Rock"
 //Return null if invalid parameters
 function playARound(playerChoice, computerChoice) {
-    //Convert parameters to string type with first uppercase letter
-    let a = 0
-    playerChoice = playerChoice.toString()
-    playerChoice = playerChoice.slice(0,1).toUpperCase() + playerChoice.slice(1).toLowerCase()
-    computerChoice = computerChoice.toString()
-    computerChoice = computerChoice.slice(0,1).toUpperCase() + computerChoice.slice(1).toLowerCase()
-
     //Check if parameters are valid
+        //Check if parameters are of type string
+    if (typeof playerChoice !== 'string' || typeof computerChoice !== 'string')
+    {
+        return null
+    }
+        //Convert parameters to format with first uppercase letter
+    playerChoice = playerChoice.slice(0,1).toUpperCase() + playerChoice.slice(1).toLowerCase()
+    computerChoice = computerChoice.slice(0,1).toUpperCase() + computerChoice.slice(1).toLowerCase()
+        //Check if parameters are equal to one allowed moves
     let moves = ['Rock', 'Paper', 'Scissors']
     let computerChoiceValid = moves.includes(computerChoice)
     let playerChoiceValid = moves.includes(playerChoice)
@@ -25,9 +27,10 @@ function playARound(playerChoice, computerChoice) {
         return null;
     }
 
-    return playerChoice + ' ' + computerChoice
+    //Return a string with a result
+    //TODO
+    return playerChoice + '  vs ' + computerChoice
 
-    
 }
 
 //Function that plays 5 rounds of the game, prompting for user choice and console.logging the result
